67ef545281acb4c62c821bcc7cb181df8622675c
LORIEUL Titouan <titouan.lorieul@inria.fr>
2024-06-07 12:32:27 +0200

    Created EventFormatter

--- scripts/formatter.js
+++ scripts/formatter.js
@@ -1,3 +1,40 @@
+class EventFormatter {
+	static format(event) {
+		const startTime = this.#formatTime(event["startDate"]);
+		const endTime = this.#formatTime(event["endDate"]);
+		const title = this.#formatTitle(event["title"]);
+		const description = this.#formatDescription(event["description"]);
+
+		const event_tag = `
+<evenement>
+<heure>${startTime}-${endTime}</heure>
+<titre>${title}</titre>
+<description>${description}</description>
+</evenement>
+		`;
+		return event_tag;
+	}
+
+	static #formatTime(time) {
+		// Formats to HH:MM (09:30, 23:59, etc.)
+		const timeOptions = {
+			"hour": "2-digit",
+			"minute": "2-digit",
+		};
+		return time.toLocaleTimeString("fr-FR", timeOptions);
+	}
+
+	static #formatTitle(title) {
+		return title;
+	}
+
+	// TODO complete this
+	static #formatDescription(description) {
+		// Remove most HTML tags
+		return strip_tags(description, "<b><strong><i><em><u>");
+	}
+}
+
 function event_formatter(event) {
 	options = {
 		"weekday": "long"
@@ -5,24 +42,11 @@ function event_formatter(event) {
 	dateDayOfWeek = new Intl.DateTimeFormat("fr-FR", options).format(event["startDate"]);
 	date = event["startDate"].getDate();
 
-	timeOptions = {
-		"hour": "2-digit",
-		"minute": "2-digit",
-	};
-	startHour = event["startDate"].toLocaleTimeString("fr-FR", timeOptions);
-	endHour = event["endDate"].toLocaleTimeString("fr-FR", timeOptions);
-
-	title = event["title"];
-	description = strip_tags(event["description"], "<b><strong><i><em><u>");
-
+	event_fmt = EventFormatter.format(event);
 	event_tag = $(`
 <div>
 <date>${dateDayOfWeek} <jour>${date}</jour></date>
-<evenement>
-<heure>${startHour}-${endHour}</heure>
-<titre>${title}</titre>
-<description>${description}</description>
-</evenement>
+${event_fmt}
 </div>
 			 `);
 
