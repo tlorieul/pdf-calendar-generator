8384bdd328815d1b53f30214bd99ee68787f98ce
LORIEUL Titouan <titouan.lorieul@inria.fr>
2024-06-07 14:25:13 +0200

    Added support for grouping of events by date

--- Readme.md
+++ Readme.md
@@ -1,6 +1,7 @@
 # Todo
-[ ] Récupérer le mois automatiquement
-[ ] Regrouper les évènements par jour
+[X] Regrouper les évènements par jour
 [ ] Tronquer la longueur du message
-[ ] Compléter automatiquement le formulaire de génération avec les dates du mois en cours
 [ ] Pagination selon la semaine ou la place disponible sur la page
+[ ] Récupérer le mois automatiquement
+[ ] Vérifier la validité des dates du formulaire
+[ ] Compléter automatiquement le formulaire de génération avec les dates du mois en cours
--- scripts/formatter.js
+++ scripts/formatter.js
@@ -35,20 +35,69 @@ class EventFormatter {
 	}
 }
 
-function event_formatter(event) {
-	options = {
-		"weekday": "long"
-	};
-	dateDayOfWeek = new Intl.DateTimeFormat("fr-FR", options).format(event["startDate"]);
-	date = event["startDate"].getDate();
+function events_by_date_formatter(events) {
+	events_by_date = new Map();
+	for(const event of events) {
+		date = event["startDate"].getDate();
+		
+		if(events_by_date.get(date) === undefined)
+			events_by_date.set(date, []);
+		
+		events_by_date.get(date).push(event);
+	}
 
-	event_fmt = EventFormatter.format(event);
-	event_tag = $(`
+	
+	events_html = []
+	
+	for(const events of events_by_date.values()) {
+		event = events[0];
+		options = {
+			"weekday": "long"
+		};
+		dateDayOfWeek = new Intl.DateTimeFormat("fr-FR", options).format(event["startDate"]);
+		date = event["startDate"].getDate();
+
+		events_fmt = [];
+		
+		for(const event of events) {
+			event_fmt = EventFormatter.format(event);
+			events_fmt.push(event_fmt);
+		}
+		events_str = events_fmt.join("");
+
+		day_events_html = `
+<day-events-block>
+<date-block>${dateDayOfWeek} <date>${date}</date></date-block>
+${events_str}
+</day-events-block>
+			 `;
+		events_html.push(day_events_html);
+	}
+
+	return events_html;
+}
+
+
+function event_formatter(event) {
+	result = []
+	
+	for(const event of events) {
+		options = {
+			"weekday": "long"
+		};
+		dateDayOfWeek = new Intl.DateTimeFormat("fr-FR", options).format(event["startDate"]);
+		date = event["startDate"].getDate();
+
+		event_fmt = EventFormatter.format(event);
+		event_str = `
 <day-events-block>
 <date-block>${dateDayOfWeek} <date>${date}</date></date-block>
 ${event_fmt}
 </day-events-block>
-			 `);
+			 `;
 
-	return event_tag;
+		result.push(event_str);
+	}
+
+	return result;
 }
